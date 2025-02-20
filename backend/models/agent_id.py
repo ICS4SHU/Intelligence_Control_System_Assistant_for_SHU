142507b2c5c6d351fb2a9a51c28603365fa9a5f8
irorange27 <orange27@shu.edu.cn>
2025-02-20 16:16:29 +0800

    chore: modify agent_id.py

---
+++ backend/models/agent_id.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class AgentID(Enum):
+    REVISION = "e3cfde42ed1a11ef9f240242ac120006"
+    DRIVEEXAMIN = "d8214c5ee7aa11efa80f0242ac120003"
+    HOMEWORK = "e7074ec2eace11ef8da20242ac120003"
